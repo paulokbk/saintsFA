128a22d2f306f760cfaa4677d54bff663394b813
Paulo Filho <paulokbkfilho@hotmail.com>
2023-09-04 13:47:10 -0300

    update files

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-import AppRoutes from "./Routes"
-
-export default function App() {
-	return <AppRoutes />
-}
--- src/pages/Login/styles.js
+++
@@ -1,9 +0,0 @@
-import { styled } from "@stitches/react"
-
-export const StyledLogin = styled("div", {
-	width: "100vw",
-	height: "100vh",
-	display: "flex",
-	justifyContent: "center",
-	alignItems: "center"
-})

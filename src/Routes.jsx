63623202165bb904ff7ff0696b4d737b04ff4927
Paulo Filho <paulokbkfilho@hotmail.com>
2023-09-04 14:24:56 -0300

    update route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -9,7 +9,7 @@ export default function AppRoutes() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/saintsFA" element={<Home />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>
 		</BrowserRouter>
